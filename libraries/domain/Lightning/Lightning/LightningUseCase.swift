caa5ed1869844a85b044dc6b5ccc52c52882b94a
Julio Fernandes <julio.fernandes7@gmail.com>
2024-07-03 15:31:59 -0300

    Adding Lightning domain module

---
+++ libraries/domain/Lightning/Lightning/LightningUseCase.swift
@@ -0,0 +1,38 @@
+import Foundation
+import LightningInterface
+import NetworkClientInterface
+
+final class LightningUseCase {
+    
+    private let networkClient: NetworkClient
+    private let baseURL = URL(string: "https://mempool.space/api/v1/lightning/nodes/rankings/connectivity")
+    
+    init(networkClient: NetworkClient) {
+        self.networkClient = networkClient
+    }
+    
+    static func successfullyValidation(_ data: Data, response: HTTPURLResponse) -> NodesResult {
+        guard response.statusCode == 200, let nodes = try? JSONDecoder().decode([Nodes].self, from: data) else {
+            return  .failure(.invalidData)
+        }
+        return .success(nodes)
+    }
+}
+
+extension LightningUseCase: LightningInterface {
+    func fetchNodes(completion: @escaping (NodesResult) -> Void) {
+        guard let url = baseURL else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        
+        networkClient.request(from: url) { result in
+            switch result {
+            case let .success((data, response)): 
+                completion(LightningUseCase.successfullyValidation(data, response: response))
+            case .failure: 
+                completion(.failure(.connectivity))
+            }
+        }
+    }
+}
---
+++ libraries/domain/Lightning/LightningInterface/LightningInterface.swift
@@ -0,0 +1,71 @@
+import Foundation
+
+public enum NodesError: Error {
+    case invalidURL
+    case connectivity
+    case invalidData
+}
+
+/**
+Sample: {
+ "publicKey": "03864ef025fde8fb587d989186ce6a4a186895ee44a926bfc370e2c366597a3f8f",
+ "alias": "ACINQ",
+ "channels": 2908,
+ "capacity": 36010516297,
+ "firstSeen": 1522941222,
+ "updatedAt": 1661274935,
+ "city": { // or null
+    "de": "Vancouver",
+    "en": "Vancouver",
+    "es": "Vancouver",
+    "fr": "Vancouver",
+    "ja": "バンクーバー市",
+    "pt-BR": "Vancôver",
+    "ru": "Ванкувер"
+ }
+ "country": {
+    "de": "Vereinigte Staaten",
+    "en": "United States",
+    "es": "Estados Unidos",
+    "fr": "États Unis",
+    "ja": "アメリカ",
+    "pt-BR": "EUA",
+    "ru": "США",
+    "zh-CN": "美国"
+ }
+}
+**/
+
+public struct Nodes: Equatable, Codable {
+    public let publicKey: String
+    public let alias: String
+    public let channels: Int
+    public let capacity: Int
+    public let firstSeen: Int
+    public let updatedAt: Int
+    
+    public init(
+        publicKey: String,
+        alias: String,
+        channels: Int,
+        capacity: Int,
+        firstSeen: Int,
+        updatedAt: Int
+    ) {
+        self.publicKey = publicKey
+        self.alias = alias
+        self.channels = channels
+        self.capacity = capacity
+        self.firstSeen = firstSeen
+        self.updatedAt = updatedAt
+    }
+    
+    public static func == (lhs: Nodes, rhs: Nodes) -> Bool {
+        return lhs.publicKey == rhs.publicKey
+    }
+}
+
+public protocol LightningInterface {
+    typealias NodesResult = Result<[Nodes], NodesError>
+    func fetchNodes(completion: @escaping (NodesResult) -> Void)
+}
